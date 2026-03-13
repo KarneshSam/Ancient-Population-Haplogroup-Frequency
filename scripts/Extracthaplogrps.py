60f02f67118ae108296544231bffaeb48f0a2a46
Karnesh Sampath <inf-41-2025@bioinf-serv2.cob.lu.se>
2026-03-13 03:57:35 +0100

    updated the function for load file and use of sys

--- scripts/Extracthaplogrps.py
+++ scripts/Extracthaplogrps.py
@@ -1,26 +1,33 @@
 import pandas as pd
 import numpy as np
+import os
+import sys
 
 #######################################
 # 1. LOAD DATA
 #######################################
 
 # Load the AADR annotations file, selecting only relevant columns
-columns = [
-    'Date mean in BP in years before 1950 CE [OxCal mu for a direct radiocarbon date, and average of range for a contextual date]',
-    'Group ID', 'Political Entity', 'Lat.', 'Long.',
-    'Molecular Sex', 'Y haplogroup (manual curation in terminal mutation format)', 
-    'Y haplogroup (manual curation in ISOGG format)', 'mtDNA haplogroup if >2x or published'
-     ]
-
-df = pd.read_csv(
-    "/home/inf-41-2025/BINP29/Popgenetics/Project/AADR_Annotations_2025.tsv",
-    sep="\t",
-    usecols=columns,
-    engine="python",
-    on_bad_lines="skip")
-
-print(df.head(2))
+def load_data(input_file):
+    """Load the AADR annotations file with selected columns."""
+    columns = [
+        'Date mean in BP in years before 1950 CE [OxCal mu for a direct radiocarbon date, and average of range for a contextual date]',
+        'Group ID', 'Political Entity', 'Lat.', 'Long.',
+        'Molecular Sex', 'Y haplogroup (manual curation in terminal mutation format)', 
+        'Y haplogroup (manual curation in ISOGG format)', 'mtDNA haplogroup if >2x or published'
+    ]
+    # Use try-except to catch potential errors during file loading
+    try:
+        df = pd.read_csv(
+            input_file,
+            sep="\t",
+            usecols=columns,
+            engine="python",
+            on_bad_lines="skip")
+    except Exception as e: 
+        sys.exit(f"Error loading data: {e}")
+    return df
+#print(df.head(2))
 
 #######################################
 # 2. RENAME COLUMNS
